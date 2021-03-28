d013a21a873ee98cd74c66cb43ddead8900d0789
Alpha Barry <barrymamadoualpha2@gmail.com>
2021-03-28 20:10:45 +0200

    debut du project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'maggagino-front-app';
+  title = 'Ricomputer Maggagino';
 }
